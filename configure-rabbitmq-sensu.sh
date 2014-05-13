1ae2e1e8ac82800a8023741a88dfb3f32fd9af10
Ian Unruh <ianunruh@gmail.com>
2014-05-13 02:33:00 -0500

    Reorganized RabbitMQ SSL config

--- configure-rabbitmq-sensu.sh
+++ configure-rabbitmq-sensu.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+BASE_PATH=`pwd`
+SSL_PATH=$BASE_PATH/tmp/ssl_certs
+
+cp $BASE_PATH/etc/rabbitmq/rabbitmq.config /etc/rabbitmq
+
+mkdir -p /etc/rabbitmq/ssl
+
+cp $SSL_PATH/server/key.pem /etc/rabbitmq/ssl
+cp $SSL_PATH/server/cert.pem /etc/rabbitmq/ssl
+cp $SSL_PATH/sensu_ca/cacert.pem /etc/rabbitmq/ssl
+
+service rabbitmq-server restart
+
 rabbitmqctl add_vhost /sensu
 rabbitmqctl add_user sensu monit0r
 rabbitmqctl set_permissions -p /sensu sensu ".*" ".*" ".*"
--- install-rabbitmq.sh
+++ install-rabbitmq.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
-BASE_PATH=`pwd`
-SSL_PATH=$BASE_PATH/tmp/ssl_certs
-
 curl -s http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | apt-key add -
 echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
 
 apt-get update
 apt-get install -y rabbitmq-server
-
-cp $BASE_PATH/etc/rabbitmq/rabbitmq.config /etc/rabbitmq
-
-mkdir -p /etc/rabbitmq/ssl
-
-cp $SSL_PATH/server/key.pem /etc/rabbitmq/ssl
-cp $SSL_PATH/server/cert.pem /etc/rabbitmq/ssl
-cp $SSL_PATH/sensu_ca/cacert.pem /etc/rabbitmq/ssl
 
 rabbitmq-plugins enable rabbitmq_management
 service rabbitmq-server restart
