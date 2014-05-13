a323cc512bef13ff71570d4531713c21dec14055
Ian Unruh <ianunruh@gmail.com>
2014-05-13 02:08:42 -0500

    Removed extra Redis instance for Skyline

--- install-redis.sh
+++ install-redis.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-apt-get install -y redis-server
+BASE_PATH=`pwd`
 
 echo "vm.overcommit_memory=1" > /etc/sysctl.conf
 sysctl -f
+
+apt-get install -y redis-server
+
+cp $BASE_PATH/etc/redis/redis.conf /etc/redis
+service redis-server restart
--- install-skyline.sh
+++ install-skyline.sh
@@ -6,7 +6,7 @@ useradd -b /opt skyline
 mkdir -p /var/log/skyline /var/run/skyline
 chown skyline:skyline /var/log/skyline /var/run/skyline
 
-apt-get install -y python-pip python-numpy python-scipy git redis-server
+apt-get install -y python-pip python-numpy python-scipy git
 
 pip install pandas
 pip install patsy
@@ -20,12 +20,9 @@ chown skyline:skyline src/webapp/static/dump
 
 pip install -r requirements.txt
 
+cp $BASE_PATH/etc/init/skyline* /etc/init
 cp $BASE_PATH/opt/skyline/src/settings.py /opt/skyline/src
 
-cp $BASE_PATH/etc/init/skyline* /etc/init
-cp $BASE_PATH/etc/redis/skyline.conf /etc/redis
-
-service skyline-redis start
 service skyline-horizon start
 service skyline-analyzer start
 service skyline-webapp start
