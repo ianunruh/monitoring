a6a1c28611c5d7cf152a04afa3f3a787ddbaccf5
Ian Unruh <ianunruh@gmail.com>
2014-05-12 22:15:33 -0500

    Fixed Carbon install script

--- install-graphite-carbon.sh
+++ install-graphite-carbon.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 BASE_PATH=`pwd`
 
+export DEBIAN_FRONTEND=noninteractive
+
 apt-get install -y graphite-carbon
 
 cp $BASE_PATH/etc/default/graphite-carbon /etc/default
-service graphite-carbon start
+service carbon-cache start
