21c9874cc9e30fd70208031bdfef9e5139eac104
Ian Unruh <ianunruh@gmail.com>
2014-05-13 02:40:10 -0500

    Fixed installer for sensu-metrics-relay

--- install-sensu-metrics-relay.sh
+++ install-sensu-metrics-relay.sh
@@ -2,11 +2,13 @@
 BASE_PATH=`pwd`
 TMP_PATH=$BASE_PATH/tmp
 
+apt-get install -y git ruby ruby-dev build-essential
+gem install sensu --no-ri --no-rdoc
+
 mkdir -p $TMP_PATH && cd $TMP_PATH
 
-curl -L -O https://github.com/opower/sensu-metrics-relay/archive/master.tar.gz
-tar xf master.tar.gz
-cp -R sensu-metrics-relay-master/lib/sensu/extensions/* /etc/sensu/extensions
+git clone git://github.com/opower/sensu-metrics-relay.git
+cp -R sensu-metrics-relay/lib/sensu/extensions/* /etc/sensu/extensions
 
 cp $BASE_PATH/etc/sensu/conf.d/relay.json /etc/sensu/conf.d
 
