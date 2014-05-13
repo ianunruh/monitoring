86192b412831095c289204614900bd0786898176
Ian Unruh <ianunruh@gmail.com>
2014-05-13 15:50:41 -0500

    Added installer for Elasticsearch 0.90.x
    Using older version compatible with Oculus

---
+++ install-oculus-elasticsearch.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+curl -s http://packages.elasticsearch.org/GPG-KEY-elasticsearch | apt-key add -
+echo "deb http://packages.elasticsearch.org/elasticsearch/0.90/debian stable main" > /etc/apt/sources.list.d/elasticsearch.list
+
+apt-get update
+apt-get install -y elasticsearch openjdk-7-jre-headless
+update-rc.d elasticsearch defaults
+
+service elasticsearch start
