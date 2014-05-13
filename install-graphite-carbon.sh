688207bb5c29e419e758a2759c0fa4a8fca882cc
Ian Unruh <ianunruh@gmail.com>
2014-05-13 03:29:17 -0500

    Added configuration for Carbon relay

--- install-graphite-carbon.sh
+++ install-graphite-carbon.sh
@@ -5,5 +5,13 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get install -y graphite-carbon
 
+# Enable services
 cp $BASE_PATH/etc/default/graphite-carbon /etc/default
+
+# Create service for carbon-relay
+cp $BASE_PATH/etc/carbon/relay-rules.conf /etc/carbon
+cp $BASE_PATH/etc/init.d/carbon-relay /etc/init.d
+update-rc.d carbon-relay defaults
+
 service carbon-cache start
+service carbon-relay start
