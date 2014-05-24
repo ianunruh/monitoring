2fd0e1856a6e4f1bc9896f6012b008a7c241b154
Ian Unruh <ianunruh@gmail.com>
2014-05-23 22:52:40 -0400

    Fixed issue with Graphite SQLite permissions

--- install-graphite-web.sh
+++ install-graphite-web.sh
@@ -4,7 +4,7 @@ BASE_PATH=`pwd`
 apt-get install -y graphite-web apache2 apache2-mpm-worker libapache2-mod-wsgi
 
 chown _graphite /var/lib/graphite
-graphite-manage syncdb --noinput
+sudo -u _graphite graphite-manage syncdb --noinput
 
 rm /etc/apache2/sites-enabled/000-default.conf
 cp $BASE_PATH/etc/apache2/sites-enabled/graphite.conf /etc/apache2/sites-enabled
