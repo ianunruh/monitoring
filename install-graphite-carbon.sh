532b3cbb4f22d3cca902b00f894cd6bb21d020e5
Ian Unruh <ianunruh@gmail.com>
2014-05-25 16:14:54 -0400

    Added Carbon retention policy for Statsd

--- install-graphite-carbon.sh
+++ install-graphite-carbon.sh
@@ -14,11 +14,12 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get install -y graphite-carbon
 
-# Enable services
+cp $BASE_PATH/etc/carbon/relay-rules.conf /etc/carbon
+cp $BASE_PATH/etc/carbon/storage-schema.conf /etc/carbon
+
 cp $BASE_PATH/etc/default/graphite-carbon /etc/default
 
 # Create service for carbon-relay
-cp $BASE_PATH/etc/carbon/relay-rules.conf /etc/carbon
 cp $BASE_PATH/etc/init.d/carbon-relay /etc/init.d
 update-rc.d carbon-relay defaults
 
