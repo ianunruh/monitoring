0fdf24f94ee23a1431d7c3957111f8894caa3089
Ian Unruh <ianunruh@gmail.com>
2014-05-24 22:15:35 -0400

    Added script to install statsd

---
+++ install-statsd.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+BASE_PATH=`pwd`
+
+add-apt-repository -y ppa:chris-lea/node.js
+
+apt-get update
+apt-get install -y git nodejs
+
+useradd -s /bin/false -d /var/lib/statsd -m statsd
+
+mkdir -p /var/log/statsd
+chown statsd:statsd /var/log/statsd
+
+git clone git://github.com/etsy/statsd.git /usr/share/statsd
+
+mkdir -p /etc/statsd
+
+cp -R $BASE_PATH/etc/statsd /etc
+cp $BASE_PATH/etc/init/statsd.conf /etc/init
+
+start statsd
