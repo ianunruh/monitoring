96ed2ac7769b262f0575800987f310feb15862da
Ian Unruh <ianunruh@gmail.com>
2014-08-02 17:42:13 -0400

    Bumped Heka version
    Fixed breaking change in Heka 0.6.x

--- install-heka.sh
+++ install-heka.sh
@@ -7,8 +7,8 @@ BASE_PATH=`pwd`
 
 cd /tmp
 
-curl -OL https://github.com/mozilla-services/heka/releases/download/v0.5.2/heka_0.5.2_amd64.deb
-dpkg -i heka_0.5.2_amd64.deb
+curl -OL https://github.com/mozilla-services/heka/releases/download/v0.6.0/heka_0.6.0_amd64.deb
+dpkg -i heka_0.6.0_amd64.deb
 
 useradd -d /var/cache/hekad -m hekad
 
