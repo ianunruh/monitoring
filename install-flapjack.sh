9fd0eb9c505838875f3767cf6a23704cf7c7e68d
Ian Unruh <ianunruh@gmail.com>
2014-09-29 00:11:43 -0500

    Updated Flapjack to 1.0
    Fixes #7

--- install-flapjack.sh
+++ install-flapjack.sh
@@ -11,11 +11,11 @@
 ##
 BASE_PATH=`pwd`
 
-echo "deb http://packages.flapjack.io/deb precise main" > /etc/apt/sources.list.d/flapjack.list
+apt-key adv --keyserver keys.gnupg.net --recv 803709B6
+echo "deb http://packages.flapjack.io/deb/v1 trusty main" > /etc/apt/sources.list.d/flapjack.list
 
 apt-get update
-# Ignore unauthenticated package prompt with --force-yes
-apt-get install -y --force-yes flapjack
+apt-get install -y flapjack
 
 cp $BASE_PATH/etc/flapjack/flapjack_config.yaml /etc/flapjack
 
