611bc93876adfa3ff7ebd5ef48d12c8fc02389d7
Ian Unruh <ianunruh@gmail.com>
2015-05-12 23:48:43 -0700

    Remove unnecessary RabbitMQ restart

--- install-rabbitmq.sh
+++ install-rabbitmq.sh
@@ -9,10 +9,9 @@
 set -eux
 
 curl -s http://www.rabbitmq.com/rabbitmq-signing-key-public.asc | apt-key add -
-echo "deb http://www.rabbitmq.com/debian/ testing main" > /etc/apt/sources.list.d/rabbitmq.list
+echo "deb http://www.rabbitmq.com/debian testing main" > /etc/apt/sources.list.d/rabbitmq.list
 
 apt-get update -q
 apt-get install -yq rabbitmq-server
 
 rabbitmq-plugins enable rabbitmq_management
-service rabbitmq-server restart
